f18d791cfa9453977f3a5e5f1dbdb646297bf78b
Juan Jose <juanjo5792@gmail.com>
2022-05-13 17:05:22 -0500

    Fix name

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="w-screen h-screen flex flex-col items-center justify-center bg-black text-white text-xl gap-2">
     <p> <span class="font-bold">Miguel Angel:</span> Secretaria</p>
     <p> <span class="font-bold">Harrison Esteban:</span> Indi</p>
-    <p> <span class="font-bold">Juan Camilo:</span> No puede serrr</p>
+    <p> <span class="font-bold">Juan Carlos:</span> No puede serrr</p>
     <p> <span class="font-bold">Juanchita:</span> Cibersex</p>
   </div>
 </template>
